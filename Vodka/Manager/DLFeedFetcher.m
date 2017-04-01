96fe5a09aa695208f2cf9c3a9ddcc00422e5d57a
DingLin <dinglin1991@yeah.net>
2017-04-01 08:58:12 +0800

    修改

--- Vodka/Manager/DLFeedFetcher.m
+++ Vodka/Manager/DLFeedFetcher.m
@@ -29,34 +29,14 @@
 
 }
 
-//加载RSS列表，并缓存
+//从缓存中加载RSS列表
 -(void)loadRSSList {
-    //请求RSS的种类
-    [XMCenter sendRequest:^(XMRequest *request) {
-        request.api = @"classes/DLRSS";
-        request.parameters = @{};
-        request.headers = @{};
-        request.httpMethod = kXMHTTPMethodGET;
-        request.requestSerializerType = kXMRequestSerializerJSON;
-    } onSuccess:^(id responseObject) {
-        
-        NSError *error;
-        NSArray <DLRSS *>*RSSList = [DLRSS arrayOfModelsFromDictionaries:responseObject[@"results"] error:&error];
-        
-        //缓存到sqlite，findOrCreate
-        dispatch_async(_sqliteQueue, ^{
-        
-            
-            
-        });
-        
-        
-    } onFailure:^(NSError *error) {
-        NSLog(@"onFailure: %@", error);
-    } onFinished:^(id responseObject, NSError *error) {
-        NSLog(@"onFinished");
-    }];
 
+    dispatch_async(_sqliteQueue, ^{
+        
+        
+        
+    });
 
 }
 
@@ -67,23 +47,13 @@
         self.onStartLoadFeeds();
     }
     
-    //加载RSS列表，并缓存到数据库
     [self loadRSSList];
     
     //遍历RSS,请求相应的feeds
     dispatch_async(_sqliteQueue, ^{
         
-        //查询数据库，获取RSSList
-        
-        
         //根据RSS，创建NSOperation，请求相应的feeds
-        
-        
-        
-        
-        
-        
-        
+
     });
     
 
